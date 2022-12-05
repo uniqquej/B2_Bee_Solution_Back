9672cebfb15ab2a724d03346eba773aebf380c68
uniqquej <cjy4477@gmail.com>
2022-12-05 10:29:58 +0900

    modify: category 모델 수정

--- article/models.py
+++ article/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 from users.models import User
 
-class Category(models.Model):
-    category = models.CharField(max_length=50)
-    
 class Solution(models.Model):
     rating = models.ManyToManyField(User,related_name='sl_rating')
     output_image = models.ImageField(null=True)
     
 class Article(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    category = models.CharField(max_length=10)
     solution = models.ForeignKey(Solution,on_delete=models.CASCADE)    
     content = models.TextField()
     mbti = models.CharField(max_length=50)
