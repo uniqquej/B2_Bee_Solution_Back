747b62ca6eeb94e012dd66ad5a8ee5672938688d
uniqquej <cjy4477@gmail.com>
2022-12-05 10:28:19 +0900

    add: 유저기반 협업필터링 close #5

--- article/views.py
+++ article/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from similarity import make_solution
+
+class SolutionView(APIView):
+    def get(self, request):
+
+        my_id = request.user.id
+        result = make_solution(my_id)
+        print(result)
+        
+        return Response({"message":"성공", "solution_id":result})
