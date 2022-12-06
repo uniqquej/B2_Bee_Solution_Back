03655577d456d92b074fb0da31a8828fa9899984
uniqquej <cjy4477@gmail.com>
2022-12-06 17:23:59 +0900

    modify: pull sync 맞추기

--- article/serializers.py
+++ article/serializers.py
@@ -33,5 +33,5 @@ class RatingSerializer(serializers.ModelSerializer):
 class MakeSolutionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Solution
-        fields = ['user','solution_image']
+        fields = ['user','solution_image', 'wise']
         read_only_fields=['user',]
