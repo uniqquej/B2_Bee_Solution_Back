a82929cf5b0badb5f169385694876ae4de0f0076
leehyewon <tnddl2345@gmail.com>
2022-12-28 23:55:39 +0900

    style: 코드 formatting message #100

--- message/urls.py
+++ message/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from message import views
 
 urlpatterns = [
-    path('<int:check>/', views.MessageView.as_view(), name = 'message_view'),
+    path('<int:check>/', views.MessageView.as_view(), name='message_view'),
     path('detail/<int:message_id>/',views.DetailMessageView.as_view(), name='detail_message_view')
 ]
